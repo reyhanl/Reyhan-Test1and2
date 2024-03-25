4a0c20c91a6a961fc8f959bfd8ccdb5f4a05798d
reyhan muhammad <reyhanmuhammad@reyhans-MacBook-Air.local>
2024-03-25 09:55:21 +0700

    add basic qr scanner

--- Test-Reyhan/ScanQRViewController.swift
+++ Test-Reyhan/ScanQRViewController.swift
@@ -10,4 +10,78 @@ import AVKit
 
 class ScanQRViewController: UIViewController, CustomTransitionEnabledVC{
     
+    var interactionController: UIPercentDrivenInteractiveTransition?
+    var customTransitionDelegate: TransitioningManager = TransitioningManager()
+    let session = AVCaptureSession()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupCamera()
+    }
+    
+    private enum Constants {
+      static let alertTitle = "Scanning is not supported"
+      static let alertMessage = "Your device does not support scanning a code from an item. Please use a device with a camera."
+      static let alertButtonTitle = "OK"
+    }
+
+    // MARK: - set up camera
+
+    func setupCamera() {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: device)
+            
+            let output = AVCaptureMetadataOutput()
+
+            output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            
+            session.addInput(input)
+            session.addOutput(output)
+            
+            output.metadataObjectTypes = [.qr]
+            
+            let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+            previewLayer.videoGravity = .resizeAspectFill
+            previewLayer.frame = view.bounds
+            
+            view.layer.addSublayer(previewLayer)
+            
+            session.startRunning()
+        } catch {
+
+            showAlert()
+            print(error)
+        }
+    }
+
+    // MARK: - Alert
+
+    func showAlert() {
+        let alert = UIAlertController(title: Constants.alertTitle,
+                                      message: Constants.alertMessage,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: Constants.alertButtonTitle,
+                                      style: .default))
+        present(alert, animated: true)
+    }
+}
+
+extension ScanQRViewController: AVCaptureMetadataOutputObjectsDelegate {
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput,
+                        didOutput metadataObjects: [AVMetadataObject],
+                        from connection: AVCaptureConnection) {
+
+        guard let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
+                  metadataObject.type == .qr,
+                  let stringValue = metadataObject.stringValue else { return }
+        
+        print(stringValue)
+    }
 }
--- Test-Reyhan/ViewController.swift
+++ Test-Reyhan/ViewController.swift
@@ -7,17 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CustomTransitionEnabledVC {
+    
+    var interactionController: UIPercentDrivenInteractiveTransition?
+    
+    var customTransitionDelegate: TransitioningManager = TransitioningManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        addPanGestureRecognizer()
         // Do any additional setup after loading the view.
+    }
+    
+    func addPanGestureRecognizer(){
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handleTransition(_:)))
+        
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(panGesture)
     }
     
     func presentScanQRVC(){
         let vc = ScanQRViewController()
-        vc
+        interactionController = UIPercentDrivenInteractiveTransition()
+        vc.customTransitionDelegate.interactionController = interactionController
+        vc.transitioningDelegate = vc.customTransitionDelegate
+        vc.customTransitionDelegate.presentationTransitionType = .swipeRight
+        vc.customTransitionDelegate.dismissalTransitionType = .swipeLeft
+        vc.modalPresentationStyle = .custom
+        self.present(vc, animated: true)
     }
 
+    
+    @objc func handleTransition(_ gestureRecognizer: UIPanGestureRecognizer){
+        let translationX = gestureRecognizer.translation(in: view).x
+        let percentageInDecimal = translationX / view.frame.width
+        
+        switch gestureRecognizer.state {
+        case .began:
+            presentScanQRVC()
+        case .changed:
+            interactionController?.update(percentageInDecimal)
+        case .ended:
+            if percentageInDecimal > 0.5{
+                interactionController?.finish()
+            }else{
+                interactionController?.cancel()
+            }
+        default:
+            break
+        }
+    }
 }
 
