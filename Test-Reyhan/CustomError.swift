5bd869be1ca2742e3161a157c7ef48f55cfcabbf
reyhan muhammad <reyhanmuhammad@reyhans-MacBook-Air.local>
2024-03-25 14:49:09 +0700

    add CoreData helper

--- Test-Reyhan/CustomError.swift
+++ Test-Reyhan/CustomError.swift
@@ -9,6 +9,9 @@ import Foundation
 
 enum CustomError: String, Error{
     case unableToScanQRCode
+    case somethingWentWrong
+    case userNotFound
+    case insufficientBalance
 }
 
 extension CustomError: LocalizedError {
@@ -16,6 +19,8 @@ extension CustomError: LocalizedError {
         switch self {
         case .unableToScanQRCode:
             return NSLocalizedString("QR is not in the right format", comment: "The QR code scanned is not in the right format")
+        case .insufficientBalance:
+            return NSLocalizedString("You do not have enough balance to complete the transaction", comment: "You do not have enough balance to complete the transaction")
         default:
             return NSLocalizedString("Something went wrong", comment: "Something unexpected occured")
         }
