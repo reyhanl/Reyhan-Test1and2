c54c8103906905cb9601a647c62db88cb67470d6
reyhan muhammad <artube090@gmail.com>
2024-06-01 12:24:26 +0700

    remove pod

--- Pods/Target Support Files/Pods-Test-Reyhan-Test-ReyhanUITests/Pods-Test-Reyhan-Test-ReyhanUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Test_Reyhan_Test_ReyhanUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Test_Reyhan_Test_ReyhanUITests
-@end
--- Pods/Target Support Files/Pods-Test-ReyhanTests/Pods-Test-ReyhanTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Test_ReyhanTests : NSObject
-@end
-@implementation PodsDummy_Pods_Test_ReyhanTests
-@end
